5fc7a2b6ea42461ac690334fbb8f2fa78fdb9b68
beverlybeevniz6957-collab <beverlybeevniz6957@gmail.com>
2026-09-16 01:31:38 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,12 @@
+.#include <stdio.h>
+
+int main(){
+printf("Units Offered This Semester:/n");
+printf("1. introduction to programming/n");
+printf("2. Data Structures/n");
+printf("3. Computer Networks/n");
+printf("4. Database Systems/n");
+printf("5. Software Engineering/n");
+
+return 0;
+}
